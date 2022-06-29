f45e12dd2049d34fab0f5b1fc58e9e92f98aedeb
prabhjot kaur <priya998815@gmail.com>
2022-06-29 21:28:33 +0530

    first commit

---
+++ stockapp/serializers.py
@@ -0,0 +1,34 @@
+from dataclasses import field, fields
+from pyexpat import model
+from unicodedata import category
+from .models import Product,Inventory,Category,Transaction
+from rest_framework import serializers
+
+       
+class Categoryserilizer(serializers.ModelSerializer):
+    # product = serializers.HiddenField(default = 1)
+    class Meta:
+        model = Category
+        fields = ['product','product_code','packs_per_carton']
+        depth = 0
+
+
+class Productserilizer(serializers.ModelSerializer):
+    # barcode_number = serializers.HiddenField(default = 1)
+    category_set = Categoryserilizer(many=True,read_only= True)
+    class Meta:
+        model = Product
+        fields = ['barcode_number','name','category_set']
+        
+
+class Inventoryserilizer(serializers.ModelSerializer):
+    class Meta:
+        model = Inventory
+        fields = ['product','quantity']
+        depth = 2
+
+class Transactionserilizer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+        depth = 0
